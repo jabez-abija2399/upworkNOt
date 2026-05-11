e1b179d788d8484c31af35295c955d6021600f73
jabez-abija2399 <jabeznew2399@gmail.com>
2026-05-11 20:50:33 +0300

    feat: enable monitoring of multiple Upwork RSS feeds via environment variable list

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     """
     BOT_TOKEN = os.getenv("BOT_TOKEN") 
     CHAT_ID = os.getenv("CHAT_ID")
-    UPWORK_FEED_URL = "https://www.upwork.com/ab/feed/jobs/rss?q=reactjs"
+    UPWORK_URLS = os.getenv("UPWORK_URLS", "https://www.upwork.com/ab/feed/jobs/rss?q=reactjs").split(",")
     # We read the KEYWORDS string and split it by commas into a list
     KEYWORDS = os.getenv("KEYWORDS", "react,python").split(",")
 
