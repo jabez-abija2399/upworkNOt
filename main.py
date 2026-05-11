997fe9d65704d96540c578a076048b8d2b26f8bd
jabez-abija2399 <jabeznew2399@gmail.com>
2026-05-11 20:36:49 +0300

    feat: integrate UpworkService to fetch and post latest jobs to Telegram

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-
 from config import Config 
 from logic.telegram import TelegramService 
+from services.upwork import UpworkService 
 
 def run_bot():
     Config.validate()
+    
     tg_service = TelegramService(Config.BOT_TOKEN, Config.CHAT_ID)
-    print("Starting bot...")
-    tg_service.send_message("<b>Bot Started!</b> I am now watching for jobs.")
+    upwork_service = UpworkService(Config.UPWORK_FEED_URL)
+    
+    print("Fetching jobs...")
+
+    jobs = upwork_service.fetch_latest_jobs()
+    
+    # 4. Process the jobs
+    for job in jobs:
+        message = f"<b>{job.title}</b>\n\n🔗 <a href='{job.link}'>View Job</a>"
+        tg_service.send_message(message)
+        print(f"Sent: {job.title}")
 
 if __name__ == "__main__":
     run_bot()
